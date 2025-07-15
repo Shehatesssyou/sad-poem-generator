30da2e0da4ba820e91edd5be04d121f8f2aa665e
Shehatesssyou <the.bee717@gmail.com>
2025-07-15 16:36:36 -0700

    added typewritter plugin, and basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "aw shit",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemForm = document.getElementById("poem-form");
+poemForm.addEventListener("submit", generatePoem);
